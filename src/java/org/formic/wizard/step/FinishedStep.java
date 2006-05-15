a9aae503c658799a3db568692c189dde7bebea93
ervandew <ervandew@gmail.com>
2006-05-15 03:38:38 +0000

    added stubs for next set of steps to implement
    git-svn-id: https://formic.svn.sourceforge.net/svnroot/formic/trunk@12 1a7380c6-1923-0410-bcea-c2eae4ba7d6c

---
+++ src/java/org/formic/wizard/step/FinishedStep.java
@@ -0,0 +1,61 @@
+/**
+ * Formic installer framework.
+ * Copyright (C) 2004 - 2006  Eric Van Dewoestine
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.formic.wizard.step;
+
+import java.util.Properties;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+
+/**
+ * Step to be displayed at the very end of the wizard sequence, after the
+ * application has been successfully installed.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class FinishedStep
+  extends AbstractStep
+{
+  /**
+   * Constructs this step.
+   */
+  public FinishedStep (String _name, Properties _properties)
+  {
+    super(_name, _properties);
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see org.formic.wizard.WizardStep#initGui()
+   */
+  public JComponent initGui ()
+  {
+    return new JPanel();
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see org.formic.wizard.WizardStep#initConsole()
+   */
+  public charvax.swing.JComponent initConsole ()
+  {
+    return null;
+  }
+}
