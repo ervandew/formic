8d90ae9d6ae0bfd01f63a8f1f7208e9033b9c381
ervandew <ervandew@gmail.com>
2006-06-06 02:05:28 +0000

    got a rough cut at working version of self extracting shell script
    git-svn-id: https://formic.svn.sourceforge.net/svnroot/formic/trunk@101 1a7380c6-1923-0410-bcea-c2eae4ba7d6c

--- src/java/org/formic/ant/PackageTask.java
+++ src/java/org/formic/ant/PackageTask.java
@@ -20,13 +20,21 @@ package org.formic.ant;
 
 import java.io.File;
 
+import org.apache.commons.io.FilenameUtils;
+
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 
+import org.apache.tools.ant.taskdefs.Chmod;
+import org.apache.tools.ant.taskdefs.Concat;
+import org.apache.tools.ant.taskdefs.Property;
 import org.apache.tools.ant.taskdefs.Tar;
 import org.apache.tools.ant.taskdefs.Zip;
 
 import org.apache.tools.ant.types.FileSet;
+
+import org.apache.tools.ant.types.Path.PathElement;
+
 import org.apache.tools.ant.types.ZipFileSet;
 
 /**
@@ -38,8 +46,8 @@ import org.apache.tools.ant.types.ZipFileSet;
 public class PackageTask
   extends Task
 {
-  // FIXME
-  private static final String FORMIC_HOME = ".";
+  private static final String FORMIC_HOME_PROPERTY = "formic.home";
+  private static final String FORMIC_HOME_ENV = "env.FORMIC_HOME";
 
   private static final String UNIX = "unix";
   private static final String LINUX = "linux";
@@ -47,8 +55,11 @@ public class PackageTask
 
   private String os;
   private File destFile;
+  private String archive;
   private Tar tar;
   private Zip zip;
+
+  private String formicHome;
 
   /**
    * Executes this task.
@@ -56,39 +67,58 @@ public class PackageTask
   public void execute ()
     throws BuildException
   {
+    formicHome = determineFormicHome();
+
     checkOs();
     check("destfile", destFile, "");
 
     try{
       if(os.equals(LINUX) || os.equals(UNIX)){
+        File tarFile = new File(archive + ".tar.gz");
+
         Tar tar = getTar();
 
-        Tar.TarFileSet antFiles = tar.createTarFileSet();
-        antFiles.setDir(new File(FORMIC_HOME));
-        antFiles.setIncludes("ant/**/*");
-        antFiles.setExcludes("ant/bin/**/*");
+        Tar.TarFileSet files = tar.createTarFileSet();
+        files.setDir(new File(formicHome));
+        files.setIncludes("ant/**/*");
+        files.setExcludes("ant/bin/**/*");
 
-        Tar.TarFileSet antBinFiles = tar.createTarFileSet();
-        antBinFiles.setDir(new File(FORMIC_HOME));
-        antBinFiles.setMode("755");
-        antBinFiles.setIncludes("ant/bin/ant");
+        Tar.TarFileSet binFiles = tar.createTarFileSet();
+        binFiles.setDir(new File(formicHome));
+        binFiles.setMode("755");
+        binFiles.setIncludes("ant/bin/ant");
 
-        tar.setDestFile(destFile);
+        Tar.TarFileSet formicFiles = tar.createTarFileSet();
+        formicFiles.setDir(new File(formicHome + "/shell"));
+        formicFiles.setMode("755");
+        formicFiles.setIncludes("formic");
+
+        tar.setDestFile(tarFile);
         tar.setTaskName("tar");
         tar.setProject(getProject());
         tar.execute();
+
+        buildSelfExtractingShellScript(tarFile);
       }else{
+        File zipFile = new File(archive + ".zip");
         Zip zip = getZip();
 
-        FileSet antFiles = new FileSet();
-        antFiles.setDir(new File(FORMIC_HOME));
-        antFiles.setIncludes("ant/**/*");
-        zip.addFileset(antFiles);
+        FileSet files = new FileSet();
+        files.setDir(new File(formicHome));
+        files.setIncludes("ant/**/*");
+        zip.addFileset(files);
 
-        zip.setDestFile(destFile);
+        FileSet formicFiles = new FileSet();
+        formicFiles.setDir(new File(formicHome + "/shell"));
+        formicFiles.setIncludes("shell/formic.bat");
+        zip.addFileset(formicFiles);
+
+        zip.setDestFile(zipFile);
         zip.setTaskName("zip");
         zip.setProject(getProject());
         zip.execute();
+
+        buildWindowsExecutable(zipFile);
       }
     }catch(BuildException be){
       throw be;
@@ -115,6 +145,15 @@ public class PackageTask
    */
   public void setDestFile (File destFile)
   {
+    String path = FilenameUtils.getFullPath(destFile.getAbsolutePath());
+    String file = FilenameUtils.getName(destFile.getAbsolutePath());
+
+    if(file.endsWith(".sh")){
+      this.archive = path + file.substring(0, file.lastIndexOf('.'));
+    }else{
+      this.archive = path + file;
+    }
+
     this.destFile = destFile;
   }
 
@@ -145,9 +184,42 @@ public class PackageTask
   }
 
   /**
+   * Determines the location of the formic distribution based on the current
+   * settings.
+   *
+   * @return The formic home directory.
+   */
+  private String determineFormicHome ()
+    throws BuildException
+  {
+    // first try ant property.
+    String home = getProject().getProperty(FORMIC_HOME_PROPERTY);
+
+    // attempt to locate environment variable.
+    if(home == null){
+      Property property = new Property();
+      property.setEnvironment("env");
+      property.setProject(getProject());
+      property.execute();
+
+      home = getProject().getProperty(FORMIC_HOME_ENV);
+    }
+
+    if(home == null){
+      throw new BuildException(
+          "Unable to determine location of formic distribution:  " +
+          "Property 'formic.home' not set. " +
+          "Environment variable 'FORMIC_HOME' not found.");
+    }
+
+    return home;
+  }
+
+  /**
    * Validate that the OS was supplied and that it is valid.
    */
   private void checkOs ()
+    throws BuildException
   {
     check("os", os,
       "The target OS must be specified.\n" +
@@ -171,11 +243,49 @@ public class PackageTask
    * @param _message The message to display.
    */
   private void check (String _name, Object _value, String _message)
+    throws BuildException
   {
     if (_value == null){
       throw new BuildException(
           "Attribute '" + _name + "' is required.\n" + _message);
     }
+  }
+
+  /**
+   * Builds a self extracting shell script for *nix operating systems.
+   */
+  private void buildSelfExtractingShellScript (File archive)
+    throws BuildException
+  {
+    Concat concat = new Concat();
+    concat.setTaskName("concat");
+    concat.setProject(getProject());
+    concat.setDestfile(destFile);
+    concat.setBinary(true);
+
+    PathElement script = concat.createPath().createPathElement();
+    script.setLocation(new File(formicHome + "/shell/selfextract"));
+
+    PathElement tarFile = concat.createPath().createPathElement();
+    tarFile.setLocation(archive);
+
+    concat.execute();
+
+    // set the file as executable
+    Chmod chmod = new Chmod();
+    chmod.setProject(getProject());
+    chmod.setFile(destFile);
+    chmod.setPerm("755");
+
+    chmod.execute();
+  }
+
+  /**
+   * Builds an executable for Windows.
+   */
+  private void buildWindowsExecutable (File archive)
+    throws BuildException
+  {
   }
 
 // Tar delegation methods.
