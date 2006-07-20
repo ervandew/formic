a506afaede6fad3f61540a1b63f4017d1bfe3629
ervandew <ervandew@gmail.com>
2006-07-20 01:24:03 +0000

    moved installer scripts to resources and made some headway on integrating launch4j to create windows executables
    git-svn-id: https://formic.svn.sourceforge.net/svnroot/formic/trunk@104 1a7380c6-1923-0410-bcea-c2eae4ba7d6c

--- src/java/org/formic/ant/PackageTask.java
+++ src/java/org/formic/ant/PackageTask.java
@@ -20,21 +20,31 @@ package org.formic.ant;
 
 import java.io.File;
 
+import java.lang.reflect.Method;
+
 import org.apache.commons.io.FilenameUtils;
 
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.ComponentHelper;
+import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
 
 import org.apache.tools.ant.taskdefs.Chmod;
 import org.apache.tools.ant.taskdefs.Concat;
+import org.apache.tools.ant.taskdefs.Copy;
+import org.apache.tools.ant.taskdefs.Jar;
+import org.apache.tools.ant.taskdefs.Mkdir;
 import org.apache.tools.ant.taskdefs.Property;
+import org.apache.tools.ant.taskdefs.Replace;
 import org.apache.tools.ant.taskdefs.Tar;
+import org.apache.tools.ant.taskdefs.Taskdef;
 import org.apache.tools.ant.taskdefs.Zip;
 
 import org.apache.tools.ant.types.FileSet;
 
 import org.apache.tools.ant.types.Path.PathElement;
 
+import org.apache.tools.ant.types.Path;
 import org.apache.tools.ant.types.ZipFileSet;
 
 /**
@@ -47,7 +57,9 @@ public class PackageTask
   extends Task
 {
   private static final String FORMIC_HOME_PROPERTY = "formic.home";
+  private static final String FORMIC_BUILDDIR_PROPERTY = "formic.builddir";
   private static final String FORMIC_HOME_ENV = "env.FORMIC_HOME";
+  private static final String DEFAULT_BUILDDIR = "build/formic";
 
   private static final String UNIX = "unix";
   private static final String LINUX = "linux";
@@ -60,6 +72,23 @@ public class PackageTask
   private Zip zip;
 
   private String formicHome;
+  private String buildDir;
+
+  /**
+   * {@inheritDoc}
+   * @see Task#init()
+   */
+  public void init ()
+  {
+    formicHome = determineFormicHome();
+    buildDir = determineBuildDir();
+
+    Mkdir mkdir = new Mkdir();
+    mkdir.setProject(getProject());
+    mkdir.setTaskName(getTaskName());
+    mkdir.setDir(new File(buildDir));
+    mkdir.execute();
+  }
 
   /**
    * Executes this task.
@@ -67,8 +96,6 @@ public class PackageTask
   public void execute ()
     throws BuildException
   {
-    formicHome = determineFormicHome();
-
     checkOs();
     check("destfile", destFile, "");
 
@@ -95,7 +122,7 @@ public class PackageTask
         formicFiles.setIncludes("formic");
 
         tar.setDestFile(tarFile);
-        tar.setTaskName("tar");
+        tar.setTaskName(getTaskName());
         tar.setProject(getProject());
         tar.execute();
 
@@ -117,7 +144,7 @@ public class PackageTask
         zip.addFileset(formicFiles);
 
         zip.setDestFile(zipFile);
-        zip.setTaskName("zip");
+        zip.setTaskName(getTaskName());
         zip.setProject(getProject());
         zip.execute();
 
@@ -148,13 +175,11 @@ public class PackageTask
    */
   public void setDestFile (File destFile)
   {
-    String path = FilenameUtils.getFullPath(destFile.getAbsolutePath());
     String file = FilenameUtils.getName(destFile.getAbsolutePath());
-
-    if(file.endsWith(".sh")){
-      this.archive = path + file.substring(0, file.lastIndexOf('.'));
+    if(file.endsWith(".sh") || file.endsWith(".exe")){
+      this.archive = buildDir + "/" + file.substring(0, file.lastIndexOf('.'));
     }else{
-      this.archive = path + file;
+      this.archive = buildDir + "/" + file;
     }
 
     this.destFile = destFile;
@@ -219,6 +244,19 @@ public class PackageTask
   }
 
   /**
+   * Determines the build directory.
+   *
+   * @return The build directory.
+   */
+  private String determineBuildDir ()
+    throws BuildException
+  {
+    // first try ant property.
+    String dir = getProject().getProperty(FORMIC_BUILDDIR_PROPERTY);
+    return dir != null ? dir : DEFAULT_BUILDDIR;
+  }
+
+  /**
    * Validate that the OS was supplied and that it is valid.
    */
   private void checkOs ()
@@ -261,7 +299,7 @@ public class PackageTask
     throws BuildException
   {
     Concat concat = new Concat();
-    concat.setTaskName("concat");
+    concat.setTaskName(getTaskName());
     concat.setProject(getProject());
     concat.setDestfile(destFile);
     concat.setBinary(true);
@@ -289,6 +327,125 @@ public class PackageTask
   private void buildWindowsExecutable (File archive)
     throws BuildException
   {
+    // construct bootstrap jar
+    File jar = constructBoostrapJar(archive);
+
+    // generate config
+    File config = generateLaunch4jConfig();
+
+    // execute launch4j
+    executeLaunch4j(config, jar);
+  }
+
+  /**
+   * Constructs a boostrap jar file for the windows installer.
+   *
+   * @param archive The archive containing the installer files.
+   * @return The bootstrap file.
+   */
+  private File constructBoostrapJar (File archive)
+    throws BuildException
+  {
+    File bootstrap = new File(
+        getProject().getProperty("basedir") + "/" + buildDir + "/formic-boostrap.jar");
+
+    Copy copy = new Copy();
+    copy.setProject(getProject());
+    copy.setTaskName(getTaskName());
+    copy.setTofile(bootstrap);
+    copy.setFile(new File(formicHome + "/ant/resources/formic-bootstrap.jar"));
+    copy.execute();
+
+    ZipFileSet fileset = new ZipFileSet();
+    fileset.setDir(new File(FilenameUtils.getFullPath(archive.getAbsolutePath())));
+    fileset.createInclude().setName(FilenameUtils.getName(archive.getPath()));
+
+    Jar jar = new Jar();
+    jar.setTaskName(getTaskName());
+    jar.setProject(getProject());
+    jar.setDestFile(bootstrap);
+    jar.setUpdate(true);
+    jar.addZipfileset(fileset);
+    jar.execute();
+
+    return bootstrap;
+  }
+
+  /**
+   * Generates a launch4j config file for building the executable.
+   *
+   * @return The generated config file.
+   */
+  private File generateLaunch4jConfig ()
+    throws BuildException
+  {
+    File launch4jConfig = new File(buildDir + "/launch4j.config.xml");
+
+    Copy copy = new Copy();
+    copy.setTaskName(getTaskName());
+    copy.setProject(getProject());
+    copy.setTofile(launch4jConfig);
+    copy.setFile(new File(formicHome + "/ant/resources/launch4j.config.xml"));
+    copy.execute();
+
+    /*Replace replace = new Replace();
+    replace.setProject(getProject());
+    replace.setFile(launch4jConfig);
+    replace.execute();*/
+
+    return launch4jConfig;
+  }
+
+  /**
+   * Executes the launch4j task to generate the windows executable.
+   *
+   * @param config The launch4j config file to use.
+   * @param jar The jar file to be executed.
+   */
+  private void executeLaunch4j (File config, File jar)
+    throws BuildException
+  {
+    // set location of launch4j distribution
+    Property property = new Property();
+    property.setName("launch4j.dir");
+    property.setLocation(new File(formicHome + "/ant/resources/launch4j"));
+    property.setProject(getProject());
+    property.execute();
+
+    // define launch4j task
+    Path classpath = new Path(getProject());
+    classpath.createPathElement().setPath(
+        getProject().getProperty("launch4j.dir") + "/launch4j.jar");
+    classpath.createPathElement().setPath(
+        getProject().getProperty("launch4j.dir") + "/lib/xstream.jar");
+
+    Taskdef task = new Taskdef();
+    task.setProject(getProject());
+    task.setName("launch4j");
+    task.setClassname("net.sf.launch4j.ant.Launch4jTask");
+    task.setClasspath(classpath);
+    task.execute();
+
+    // execute launch4j task
+    try{
+      Task launch4j = (Task)ComponentHelper.getComponentHelper(getProject())
+        .getComponentClass("launch4j").newInstance();
+      Method setConfigFile = launch4j.getClass().getDeclaredMethod(
+          "setConfigFile", new Class[]{File.class});
+      setConfigFile.invoke(launch4j, new Object[]{config});
+
+      Method setJar = launch4j.getClass().getDeclaredMethod(
+          "setJar", new Class[]{File.class});
+      setJar.invoke(launch4j, new Object[]{jar});
+
+      Method setOutfile = launch4j.getClass().getDeclaredMethod(
+          "setOutfile", new Class[]{File.class});
+      setOutfile.invoke(launch4j, new Object[]{destFile});
+
+      launch4j.execute();
+    }catch(Exception e){
+      throw new BuildException(e);
+    }
   }
 
 // Tar delegation methods.
