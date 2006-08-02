36a6a1b89df6d26427a0bbb45fd2782bd1bd41b7
ervandew <ervandew@gmail.com>
2006-08-02 18:02:40 +0000

    moved logger to logger package
    git-svn-id: https://formic.svn.sourceforge.net/svnroot/formic/trunk@130 1a7380c6-1923-0410-bcea-c2eae4ba7d6c

---
+++ src/java/org/formic/ant/logger/Log4jLogger.java
@@ -0,0 +1,62 @@
+/**
+ * Formic installer framework.
+ * Copyright (C) 2005 - 2006  Eric Van Dewoestine
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.formic.ant.logger;
+
+import java.io.PrintStream;
+
+import org.apache.tools.ant.DefaultLogger;
+import org.apache.tools.ant.Project;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Log via log4j.
+ *
+ * @author Eric Van Dewoestine (ervandew@yahoo.com)
+ * @version $Revision$
+ */
+public class Log4jLogger
+  extends DefaultLogger
+{
+  private static final Logger logger = LoggerFactory.getLogger("ANT");
+
+  /**
+   * {@inheritDoc}
+   * @see DefaultLogger#printMessage(String,PrintStream,int)
+   */
+  protected void printMessage (String message, PrintStream out, int priority)
+  {
+    switch (priority){
+      case Project.MSG_ERR:
+        logger.error(message);
+        break;
+      case Project.MSG_WARN:
+        logger.warn(message);
+        break;
+      case Project.MSG_INFO:
+        logger.info(message);
+        break;
+      case Project.MSG_DEBUG:
+      case Project.MSG_VERBOSE:
+        logger.debug(message);
+        break;
+    }
+  }
+}
